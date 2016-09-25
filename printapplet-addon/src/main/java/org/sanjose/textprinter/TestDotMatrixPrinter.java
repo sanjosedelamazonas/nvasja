85816cc745fe620c31dd5579d3ffbdeee5a5db10
prubach <pawel.rubach@sorcersoft.com>
2016-09-26 00:53:33 +0200

    Text Printer

--- printapplet-addon/src/main/java/org/sanjose/textprinter/TestDotMatrixPrinter.java
+++ printapplet-addon/src/main/java/org/sanjose/textprinter/TestDotMatrixPrinter.java
@@ -14,7 +14,7 @@ public class TestDotMatrixPrinter {
 			// Open the file that is the first
 			// command line parameter
 			//FileInputStream fstream = new FileInputStream("c:\\test.txt");
-			FileInputStream fstream = new FileInputStream("c:\\vasja_caja2\\ComprobanteTxt.txt");
+			FileInputStream fstream = new FileInputStream("c:\\dev\\ComprobanteTxt.txt");
 			//FileInputStream fstream = new FileInputStream("/tmp/ComprobanteTxt.txt");
 			// Get the object of DataInputStream
 			DataInputStream in = new DataInputStream(fstream);
@@ -23,7 +23,7 @@ public class TestDotMatrixPrinter {
 			while ((strLine = br.readLine()) != null) {
 				// Print the content on the console
 				strLine = textPrinter.convert(strLine);
-				strLine1 = strLine1 + strLine;
+				strLine1 = strLine1 + strLine + "\r\n";
 				//textPrinter.addToBuffer(strLine1);
 
 			}
@@ -36,37 +36,13 @@ public class TestDotMatrixPrinter {
 		try {
 			FileWriter out = new FileWriter("lpt1");
 			//FileWriter out = new FileWriter(new File("/tmp/out"));
+			//FileWriter out = new FileWriter(new File("c:\\dev\\out.txt"));
 			out.write(strLine1);
-			out.write(0x0D); // CR
+			out.write("\f");
+			//out.write(0x0D); // CR
 			out.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 }
-
-// class that opens the printer as a file  
-// and writes "Hello World" to it  
-
-/*import java.io.*;  
-public class lpt {  
-    public static void main (String[] argv) {  
-        try {  
-            FileOutputStream os = new FileOutputStream("LPT1");  
-            //wrap stream in "friendly" PrintStream  
-            PrintStream ps = new PrintStream(os);  
-
-            //print text here  
-            ps.println("Hello world!");  
-
-            //form feed -- this is important  
-            //Without the form feed, the text will simply sit  
-            // in print buffer until something else gets printed.  
-            ps.print("\f");  
-            //flush buffer and close  
-            ps.close();  
-        } catch (Exception e) {  
-            System.out.println("Exception occurred: " + e);  
-        }  
-    }  
-} */ 
