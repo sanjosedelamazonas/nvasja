468133e8a939f323540bb170a41f4e3927422176
Pawel Rubach <pawel.rubach@gmail.com>
2018-10-24 00:40:13 +0200

    Changed getLetraDeNumero to lower case

--- sql/Create_function_scp_vsj_getLetraDeNumero.sql
+++ sql/Create_function_scp_vsj_getLetraDeNumero.sql
@@ -27,16 +27,16 @@ SELECT @lnEntero = CAST(@lnEntero/10 AS INT)
 -- Analizo las unidades
 SELECT @lcCadena =
 CASE /* UNIDADES */
-WHEN @lnUnidades = 1 AND @lnTerna = 1 THEN 'UNO ' + @lcCadena
-WHEN @lnUnidades = 1 AND @lnTerna <> 1 THEN 'UN ' + @lcCadena
-WHEN @lnUnidades = 2 THEN 'DOS ' + @lcCadena
-WHEN @lnUnidades = 3 THEN 'TRES ' + @lcCadena
-WHEN @lnUnidades = 4 THEN 'CUATRO ' + @lcCadena
-WHEN @lnUnidades = 5 THEN 'CINCO ' + @lcCadena
-WHEN @lnUnidades = 6 THEN 'SEIS ' + @lcCadena
-WHEN @lnUnidades = 7 THEN 'SIETE ' + @lcCadena
-WHEN @lnUnidades = 8 THEN 'OCHO ' + @lcCadena
-WHEN @lnUnidades = 9 THEN 'NUEVE ' + @lcCadena
+WHEN @lnUnidades = 1 AND @lnTerna = 1 THEN 'uno ' + @lcCadena
+WHEN @lnUnidades = 1 AND @lnTerna <> 1 THEN 'un ' + @lcCadena
+WHEN @lnUnidades = 2 THEN 'dos ' + @lcCadena
+WHEN @lnUnidades = 3 THEN 'tres ' + @lcCadena
+WHEN @lnUnidades = 4 THEN 'cuatro ' + @lcCadena
+WHEN @lnUnidades = 5 THEN 'cinco ' + @lcCadena
+WHEN @lnUnidades = 6 THEN 'seis ' + @lcCadena
+WHEN @lnUnidades = 7 THEN 'siete ' + @lcCadena
+WHEN @lnUnidades = 8 THEN 'ocho ' + @lcCadena
+WHEN @lnUnidades = 9 THEN 'nueve ' + @lcCadena
 ELSE @lcCadena
 END /* UNIDADES */
 -- Analizo las decenas
@@ -44,48 +44,48 @@ SELECT @lcCadena =
 CASE /* DECENAS */
 WHEN @lnDecenas = 1 THEN
 CASE @lnUnidades
-WHEN 0 THEN 'DIEZ '
-WHEN 1 THEN 'ONCE '
-WHEN 2 THEN 'DOCE '
-WHEN 3 THEN 'TRECE '
-WHEN 4 THEN 'CATORCE '
-WHEN 5 THEN 'QUINCE '
-ELSE 'DIECI' + @lcCadena
+WHEN 0 THEN 'diez '
+WHEN 1 THEN 'once '
+WHEN 2 THEN 'doce '
+WHEN 3 THEN 'trece '
+WHEN 4 THEN 'catorce '
+WHEN 5 THEN 'quince '
+ELSE 'dieci' + @lcCadena
 END
-WHEN @lnDecenas = 2 AND @lnUnidades = 0 THEN 'VEINTE ' + @lcCadena
-WHEN @lnDecenas = 2 AND @lnUnidades <> 0 THEN 'VEINTI' + @lcCadena
-WHEN @lnDecenas = 3 AND @lnUnidades = 0 THEN 'TREINTA ' + @lcCadena
-WHEN @lnDecenas = 3 AND @lnUnidades <> 0 THEN 'TREINTA Y ' + @lcCadena
-WHEN @lnDecenas = 4 AND @lnUnidades = 0 THEN 'CUARENTA ' + @lcCadena
-WHEN @lnDecenas = 4 AND @lnUnidades <> 0 THEN 'CUARENTA Y ' + @lcCadena
-WHEN @lnDecenas = 5 AND @lnUnidades = 0 THEN 'CINCUENTA ' + @lcCadena
-WHEN @lnDecenas = 5 AND @lnUnidades <> 0 THEN 'CINCUENTA Y ' + @lcCadena
-WHEN @lnDecenas = 6 AND @lnUnidades = 0 THEN 'SESENTA ' + @lcCadena
-WHEN @lnDecenas = 6 AND @lnUnidades <> 0 THEN 'SESENTA Y ' + @lcCadena
-WHEN @lnDecenas = 7 AND @lnUnidades = 0 THEN 'SETENTA ' + @lcCadena
-WHEN @lnDecenas = 7 AND @lnUnidades <> 0 THEN 'SETENTA Y ' + @lcCadena
-WHEN @lnDecenas = 8 AND @lnUnidades = 0 THEN 'OCHENTA ' + @lcCadena
-WHEN @lnDecenas = 8 AND @lnUnidades <> 0 THEN 'OCHENTA Y ' + @lcCadena
-WHEN @lnDecenas = 9 AND @lnUnidades = 0 THEN 'NOVENTA ' + @lcCadena
-WHEN @lnDecenas = 9 AND @lnUnidades <> 0 THEN 'NOVENTA Y ' + @lcCadena
+WHEN @lnDecenas = 2 AND @lnUnidades = 0 THEN 'veinte ' + @lcCadena
+WHEN @lnDecenas = 2 AND @lnUnidades <> 0 THEN 'veinti' + @lcCadena
+WHEN @lnDecenas = 3 AND @lnUnidades = 0 THEN 'treinta ' + @lcCadena
+WHEN @lnDecenas = 3 AND @lnUnidades <> 0 THEN 'treinta y ' + @lcCadena
+WHEN @lnDecenas = 4 AND @lnUnidades = 0 THEN 'cuarenta ' + @lcCadena
+WHEN @lnDecenas = 4 AND @lnUnidades <> 0 THEN 'cuarenta y ' + @lcCadena
+WHEN @lnDecenas = 5 AND @lnUnidades = 0 THEN 'cincuenta ' + @lcCadena
+WHEN @lnDecenas = 5 AND @lnUnidades <> 0 THEN 'cincuenta y ' + @lcCadena
+WHEN @lnDecenas = 6 AND @lnUnidades = 0 THEN 'sesenta ' + @lcCadena
+WHEN @lnDecenas = 6 AND @lnUnidades <> 0 THEN 'sesenta y ' + @lcCadena
+WHEN @lnDecenas = 7 AND @lnUnidades = 0 THEN 'setenta ' + @lcCadena
+WHEN @lnDecenas = 7 AND @lnUnidades <> 0 THEN 'setenta y ' + @lcCadena
+WHEN @lnDecenas = 8 AND @lnUnidades = 0 THEN 'ochenta ' + @lcCadena
+WHEN @lnDecenas = 8 AND @lnUnidades <> 0 THEN 'ochenta y ' + @lcCadena
+WHEN @lnDecenas = 9 AND @lnUnidades = 0 THEN 'noventa ' + @lcCadena
+WHEN @lnDecenas = 9 AND @lnUnidades <> 0 THEN 'noventa Y ' + @lcCadena
 ELSE @lcCadena
 END /* DECENAS */
 
 -- Analizo las centenas
 SELECT @lcCadena =
 CASE /* CENTENAS */
-WHEN @lnCentenas = 1 AND @lnUnidades = 0 AND @lnDecenas = 0 THEN 'CIEN ' +
+WHEN @lnCentenas = 1 AND @lnUnidades = 0 AND @lnDecenas = 0 THEN 'cien ' +
 @lcCadena
 WHEN @lnCentenas = 1 AND NOT(@lnUnidades = 0 AND @lnDecenas = 0) THEN
-'CIENTO ' + @lcCadena
-WHEN @lnCentenas = 2 THEN 'DOSCIENTOS ' + @lcCadena
-WHEN @lnCentenas = 3 THEN 'TRESCIENTOS ' + @lcCadena
-WHEN @lnCentenas = 4 THEN 'CUATROCIENTOS ' + @lcCadena
-WHEN @lnCentenas = 5 THEN 'QUINIENTOS ' + @lcCadena
-WHEN @lnCentenas = 6 THEN 'SEISCIENTOS ' + @lcCadena
-WHEN @lnCentenas = 7 THEN 'SETECIENTOS ' + @lcCadena
-WHEN @lnCentenas = 8 THEN 'OCHOCIENTOS ' + @lcCadena
-WHEN @lnCentenas = 9 THEN 'NOVECIENTOS ' + @lcCadena
+'ciento ' + @lcCadena
+WHEN @lnCentenas = 2 THEN 'doscientos ' + @lcCadena
+WHEN @lnCentenas = 3 THEN 'trescientos ' + @lcCadena
+WHEN @lnCentenas = 4 THEN 'cuatrocientos ' + @lcCadena
+WHEN @lnCentenas = 5 THEN 'quinientos ' + @lcCadena
+WHEN @lnCentenas = 6 THEN 'seiscientos ' + @lcCadena
+WHEN @lnCentenas = 7 THEN 'setecientos ' + @lcCadena
+WHEN @lnCentenas = 8 THEN 'ochocientos ' + @lcCadena
+WHEN @lnCentenas = 9 THEN 'novecientos ' + @lcCadena
 ELSE @lcCadena
 END /* CENTENAS */
 -- Analizo la terna
@@ -93,15 +93,15 @@ SELECT @lcCadena =
 CASE /* TERNA */
 WHEN @lnTerna = 1 THEN @lcCadena
 WHEN @lnTerna = 2 AND (@lnUnidades + @lnDecenas + @lnCentenas <> 0) THEN
-@lcCadena + ' MIL '
+@lcCadena + ' mil '
 WHEN @lnTerna = 3 AND (@lnUnidades + @lnDecenas + @lnCentenas <> 0) AND
 @lnUnidades = 1 AND @lnDecenas = 0 AND @lnCentenas = 0 THEN @lcCadena + '
-MILLON '
+millon '
 WHEN @lnTerna = 3 AND (@lnUnidades + @lnDecenas + @lnCentenas <> 0) AND
 NOT (@lnUnidades = 1 AND @lnDecenas = 0 AND @lnCentenas = 0) THEN @lcCadena
-+ ' MILLONES '
++ ' millones '
 WHEN @lnTerna = 4 AND (@lnUnidades + @lnDecenas + @lnCentenas <> 0) THEN
-@lcCadena + ' MIL MILLONES '
+@lcCadena + ' mil millones '
 ELSE ''
 END /* TERNA */
 -- Armo el retorno terna a terna
@@ -109,8 +109,8 @@ SELECT @lcRetorno = @lcCadena + @lcRetorno
 SELECT @lnTerna = @lnTerna + 1
 END /* WHILE */
 IF @lnTerna = 1
-SELECT @lcRetorno = 'CERO'
-RETURN RTRIM(@lcRetorno) + ' CON ' + LTRIM(STR(@lnFraccion,2)) + '/100'
+SELECT @lcRetorno = 'cero'
+RETURN RTRIM(@lcRetorno) + ' con ' + LTRIM(STR(@lnFraccion,2)) + '/100'
 END
 
 GO
