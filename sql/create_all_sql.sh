b5ec52a53b16db2fdcb559ee59e06a10f7d9e80c
ab <a.borkowska@gmail.com>
2019-10-05 16:36:55 -0500

    Added indexes to script that creates all

--- sql/create_all_sql.sh
+++ sql/create_all_sql.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 cat Alter_*.sql > all.sql
 echo "PRINT '1. Alter DONE'" >> all.sql
+cat Create_idx_*.sql >> all.sql
+echo "PRINT '2. Create Idx DONE'" >> all.sql
 cat Create_vsj_*.sql >> all.sql
-echo "PRINT '2. Create_vsj DONE'" >> all.sql
+echo "PRINT '3. Create_vsj DONE'" >> all.sql
 #cat Create_function_*.sql >> all.sql
 #cat Create_trigger_*.sql >> all.sql
 cat Create_usp_*.sql >> all.sql
-echo "PRINT '3. Cr usp'" >> all.sql
+echo "PRINT '4. Cr usp'" >> all.sql
 cat Create_proc_1_*.sql >> all.sql
-echo "PRINT '4. Cr proc1'" >> all.sql
+echo "PRINT '5. Cr proc1'" >> all.sql
 cat Create_proc_usp*.sql >> all.sql
-echo "PRINT '5. Cr proc_usp*; GO;'" >> all.sql
+echo "PRINT '6. Cr proc_usp*; GO;'" >> all.sql
 cat Create_proc_fun*.sql >> all.sql
 cat Create_function_scp_*.sql >> all.sql
 echo "PRINT '7. Cr function_scp'" >> all.sql
