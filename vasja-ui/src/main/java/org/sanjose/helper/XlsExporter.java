f92a5ffc9fd6f5a4d7c607f470d8b3ab36f56eeb
Paweł Rubach <pawel.rubach@gmail.com>
2023-03-11 19:57:01 +0100

    Trying to do export rendicion

---
+++ vasja-ui/src/main/java/org/sanjose/helper/XlsExporter.java
@@ -0,0 +1,251 @@
+package org.sanjose.helper;
+
+import com.vaadin.data.util.BeanItem;
+import com.vaadin.external.org.slf4j.LoggerFactory;
+import org.apache.poi.ss.usermodel.*;
+
+import org.apache.poi.xssf.usermodel.XSSFFont;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.sanjose.util.DataFilterUtil;
+import org.sanjose.util.GenUtil;
+import org.sanjose.views.sys.DestinoView;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.logging.Logger;
+
+public class XlsExporter {
+
+    private Sheet sheet = null;
+
+    private Workbook workbook = null;
+
+    protected static final com.vaadin.external.org.slf4j.Logger log = LoggerFactory.getLogger(XlsExporter.class);
+
+    public XlsExporter(String sheetName) {
+        workbook = new XSSFWorkbook();
+        sheet = workbook.createSheet(sheetName);
+        //sheet.setColumnWidth(0, 6000);
+        //sheet.setColumnWidth(1, 4000);
+    }
+
+    public void writeHeaderLine(List<String> headerCells, int rowAt) {
+        Row row = sheet.createRow(rowAt);
+        CellStyle style = getHeaderRowStyle();
+        int cellCount = 0;
+        for (String cellContent : headerCells) {
+            createCell(row, cellCount, cellContent, style);
+            cellCount++;
+        }
+    }
+
+    public void writeTitleLine(List<String> headerCells, int rowAt) {
+        Row row = sheet.createRow(rowAt);
+        CellStyle style = getTitleRowStyle();
+        int cellCount = 0;
+        for (String cellContent : headerCells) {
+            createCell(row, cellCount, cellContent, style);
+            cellCount++;
+        }
+    }
+
+    public void writeDataLine(List<String> cells, int rowAt) {
+        Row row = sheet.createRow(rowAt);
+        CellStyle style = getDataRowStyle();
+        int cellCount = 0;
+        for (String cellContent : cells) {
+            createCell(row, cellCount, cellContent, style);
+            cellCount++;
+        }
+    }
+
+
+    public int writeDataLines(Class<?> clazz, List<String> columns, List<?> objects, int firstrow) {
+        int rowCount = firstrow;
+        CellStyle style = getDataRowStyle();
+
+        for (Object pojo : objects) {
+            Row row = sheet.createRow(rowCount++);
+            int columnCount = 0;
+            for (String col : columns) {
+                createCell(row, columnCount++, getValueFromPojo(clazz, col, pojo), style);
+            }
+        }
+        return rowCount;
+    }
+
+    private void createCell(Row row, int columnCount, Object value, CellStyle style) {
+        sheet.autoSizeColumn(columnCount);
+        Cell cell = row.createCell(columnCount);
+        if (value instanceof Integer) {
+            cell.setCellValue((Integer) value);
+        } else if (value instanceof Boolean) {
+            cell.setCellValue((Boolean) value);
+        } else if (value instanceof BigDecimal) {
+            cell.setCellValue((Double) ((BigDecimal) value).doubleValue());
+            style = getCurrencyCellStyle();
+        } else if (value instanceof Long) {
+            cell.setCellValue((Long) value);
+            style = getCenterAlignedCellStyle(style);
+        } else {
+            cell.setCellValue((String) value);
+        }
+        cell.setCellStyle(style);
+    }
+
+    protected CellStyle getTitleRowStyle() {
+        CellStyle style = workbook.createCellStyle();
+        style.setFillForegroundColor(IndexedColors.GREY_25_PERCENT.getIndex());
+        style.setFillPattern(FillPatternType.FINE_DOTS);
+        XSSFFont font = ((XSSFWorkbook) workbook).createFont();
+        font.setFontName("Arial");
+        font.setFontHeightInPoints((short) 14);
+        font.setBold(true);
+        style.setFont(font);
+        style.setAlignment(HorizontalAlignment.CENTER);
+        style.setVerticalAlignment(VerticalAlignment.BOTTOM);
+        style.setBorderTop(BorderStyle.NONE);
+        style.setBorderBottom(BorderStyle.THICK);
+        style.setBorderLeft(BorderStyle.NONE);
+        style.setBorderRight(BorderStyle.NONE);
+        return style;
+    }
+
+    protected CellStyle getHeaderRowStyle() {
+        CellStyle style = workbook.createCellStyle();
+        XSSFFont font = ((XSSFWorkbook) workbook).createFont();
+        font.setFontName("Arial");
+        font.setFontHeightInPoints((short) 12);
+        font.setBold(true);
+        style.setFont(font);
+        style.setAlignment(HorizontalAlignment.CENTER);
+        style.setVerticalAlignment(VerticalAlignment.BOTTOM);
+        style.setBorderTop(BorderStyle.THIN);
+        style.setBorderBottom(BorderStyle.THICK);
+        style.setBorderLeft(BorderStyle.THIN);
+        style.setBorderRight(BorderStyle.THIN);
+        return style;
+    }
+
+    protected CellStyle getDataRowStyle() {
+        CellStyle style = workbook.createCellStyle();
+        XSSFFont font = ((XSSFWorkbook) workbook).createFont();
+        font.setFontHeight(12);
+        style.setFont(font);
+        style.setVerticalAlignment(VerticalAlignment.BOTTOM);
+        style.setBorderTop(BorderStyle.THIN);
+        style.setBorderBottom(BorderStyle.THIN);
+        style.setBorderLeft(BorderStyle.THIN);
+        style.setBorderRight(BorderStyle.THIN);
+        return style;
+    }
+
+    protected CellStyle getCenterAlignedCellStyle(CellStyle cellStyle) {
+        cellStyle.setAlignment(HorizontalAlignment.CENTER);
+        cellStyle.setVerticalAlignment(VerticalAlignment.BOTTOM);
+        cellStyle.setBorderTop(BorderStyle.NONE);
+        cellStyle.setBorderBottom(BorderStyle.NONE);
+        cellStyle.setBorderLeft(BorderStyle.NONE);
+        cellStyle.setBorderRight(BorderStyle.NONE);
+        return cellStyle;
+    }
+
+    protected CellStyle getLeftAlignedCellStyle(CellStyle cellStyle) {
+        cellStyle.setAlignment(HorizontalAlignment.LEFT);
+        cellStyle.setVerticalAlignment(VerticalAlignment.BOTTOM);
+        cellStyle.setBorderTop(BorderStyle.NONE);
+        cellStyle.setBorderBottom(BorderStyle.NONE);
+        cellStyle.setBorderLeft(BorderStyle.NONE);
+        cellStyle.setBorderRight(BorderStyle.NONE);
+        return cellStyle;
+    }
+
+    protected CellStyle getRightAlignedCellStyle(CellStyle cellStyle) {
+        cellStyle.setAlignment(HorizontalAlignment.RIGHT);
+        cellStyle.setVerticalAlignment(VerticalAlignment.BOTTOM);
+        cellStyle.setBorderTop(BorderStyle.NONE);
+        cellStyle.setBorderBottom(BorderStyle.NONE);
+        cellStyle.setBorderLeft(BorderStyle.NONE);
+        cellStyle.setBorderRight(BorderStyle.NONE);
+        return cellStyle;
+    }
+
+    private Object getValueFromPojo(Class<?> clazz, String fieldName, Object pojo) {
+        String idCol = null;
+        String colProp = null;
+        if (fieldName.contains(".")) {
+            idCol = fieldName.substring(0, fieldName.indexOf("."));
+            colProp = fieldName.substring(fieldName.indexOf(".")+1);
+        }
+        BeanItem bItem = new BeanItem(pojo);
+        Object value = null;
+        if (fieldName.contains(".")) {
+            Object idObj = bItem.getItemProperty(idCol).getValue();
+            try {
+                Method mth = idObj.getClass().getMethod("get" + colProp.substring(0,1).toUpperCase() + colProp.substring(1), new Class[] {});
+                mth.setAccessible(true);
+                value = mth.invoke(idObj);
+            } catch (NoSuchMethodException nsm) {
+                log.error("Problem getting no method found: " + fieldName + " " + "\n" + nsm.getMessage());
+            } catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+                log.error("Problem getting data from method: " + fieldName + " " + "\n" + e.getMessage());
+            }
+        } else {
+            if (bItem.getItemProperty(fieldName)!=null && !GenUtil.objNullOrEmpty(bItem.getItemProperty(fieldName).getValue()))
+                value = bItem.getItemProperty(fieldName).getValue();
+        }
+        return value;
+    }
+
+    private static String capitalize(String s) {
+        if (s.length() == 0)
+            return s;
+        return s.substring(0, 1).toUpperCase() + s.substring(1);
+    }
+
+    private Method getMethod(Class<?> clazz, String xlsColumnField) throws NoSuchMethodException {
+        Method method;
+        try {
+            method = clazz.getMethod("get" + capitalize(xlsColumnField));
+        } catch (NoSuchMethodException nme) {
+            method = clazz.getMethod(xlsColumnField);
+        }
+
+        return method;
+    }
+
+    protected Font getBoldFont() {
+        Font font = workbook.createFont();
+        font.setBold(true);
+        font.setFontHeight((short) (10 * 20));
+        font.setFontName("Calibri");
+        font.setColor(IndexedColors.BLACK.getIndex());
+        return font;
+    }
+
+    protected Font getGenericFont() {
+        Font font = workbook.createFont();
+        font.setFontHeight((short) (10 * 20));
+        font.setFontName("Calibri");
+        font.setColor(IndexedColors.BLACK.getIndex());
+        return font;
+    }
+
+    protected CellStyle getCurrencyCellStyle() {
+        CellStyle currencyStyle = workbook.createCellStyle();
+        currencyStyle.setWrapText(true);
+        DataFormat df = workbook.createDataFormat();
+        currencyStyle.setDataFormat(df.getFormat("#0.00"));
+        return currencyStyle;
+    }
+
+    public Sheet getSheet() {
+        return sheet;
+    }
+
+    public Workbook getWorkbook() {
+        return workbook;
+    }
+}
--- vasja-ui/src/main/java/org/sanjose/views/rendicion/RendicionExportXLS.java
+++ vasja-ui/src/main/java/org/sanjose/views/rendicion/RendicionExportXLS.java
@@ -1,123 +1,105 @@
 package org.sanjose.views.rendicion;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.Map;
-import java.util.Set;
-import java.util.TreeMap;
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
-import org.apache.poi.ss.usermodel.*;
-import org.apache.poi.xssf.usermodel.XSSFFont;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import com.vaadin.server.FileDownloader;
+import com.vaadin.server.Sizeable;
+import com.vaadin.server.StreamResource;
+import com.vaadin.server.UIClassSelectionEvent;
+import com.vaadin.shared.ui.window.WindowMode;
+import com.vaadin.ui.Embedded;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.Window;
+import org.sanjose.helper.XlsExporter;
+import org.sanjose.model.ScpRendicioncabecera;
+import org.sanjose.model.ScpRendiciondetalle;
+import org.sanjose.util.GenUtil;
+import org.sanjose.views.sys.PersistanceService;
 
-public class RendicionExportXLS {
+public class RendicionExportXLS extends XlsExporter {
 
-    public static void main(String[] args) {
-        Workbook workbook = new XSSFWorkbook();
+    private ScpRendicioncabecera cabecera;
 
-        Sheet sheet = workbook.createSheet("Persons");
-        sheet.setColumnWidth(0, 6000);
-        sheet.setColumnWidth(1, 4000);
+    private PersistanceService service;
 
-        Row header = sheet.createRow(0);
+    public RendicionExportXLS(ScpRendicioncabecera cabecera, PersistanceService service) {
+        super(cabecera.getTxtGlosa());
+        this.cabecera = cabecera;
+        this.service = service;
+        exportRendicion();
+        //sheet.setColumnWidth(0, 6000);
+        //sheet.setColumnWidth(1, 4000);
+    }
 
-        CellStyle headerStyle = workbook.createCellStyle();
-        headerStyle.setFillForegroundColor(IndexedColors.LIGHT_BLUE.getIndex());
-        headerStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+    public void exportRendicion() {
 
-        XSSFFont font = ((XSSFWorkbook) workbook).createFont();
-        font.setFontName("Arial");
-        font.setFontHeightInPoints((short) 16);
-        font.setBold(true);
-        headerStyle.setFont(font);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 
-        Cell headerCell = header.createCell(0);
-        headerCell.setCellValue("Name");
-        headerCell.setCellStyle(headerStyle);
+        writeTitleLine(Arrays.asList(new String[]{ cabecera.getTxtGlosa()}), 0);
 
-        headerCell = header.createCell(1);
-        headerCell.setCellValue("Age");
-        headerCell.setCellStyle(headerStyle);
+        writeDataLine(Arrays.asList(new String[]{ "Fecha de rendicion: ", sdf.format(cabecera.getFecComprobante())}), 1);
+        writeDataLine(Arrays.asList(new String[]{ "Destino", cabecera.getCodDestino()}), 2);
+        writeDataLine(Arrays.asList(new String[]{ "Moneda", GenUtil.getSymMoneda(cabecera.getCodTipomoneda())}), 3);
 
-        CellStyle style = workbook.createCellStyle();
-        style.setWrapText(true);
+        writeHeaderLine(Arrays.asList(new String[]{ "Nro", "Descripcion", "Ingreso", "Egreso"}), 5);
 
-        Row row = sheet.createRow(2);
-        Cell cell = row.createCell(0);
-        cell.setCellValue("John Smith");
-        cell.setCellStyle(style);
+        List<ScpRendiciondetalle> detalles = service.getRendiciondetalleRep().findById_CodRendicioncabecera(cabecera.getCodRendicioncabecera());
+        writeDataLines(ScpRendiciondetalle.class,
+                Arrays.asList(new String[]{"id.numNroitem", "txtGlosaitem", "numDebesol", "numHabersol"}),
+                detalles,
+                1);
 
-        cell = row.createCell(1);
-        cell.setCellValue(20);
-        cell.setCellStyle(style);
+        openExported();
+    }
 
 
+    public void openExported() {
+        StreamResource.StreamSource source = (StreamResource.StreamSource) () -> {
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            try {
+                getWorkbook().write(outputStream);
+                getWorkbook().close();
+            } catch (IOException ex) {
+                log.error("Error exporting to XLS");
 
-        Sheet spreadsheet
-                = workbook.createSheet(" Student Data ");
-
-        // creating a row object
-        //Row row;
-
-        // This data needs to be written (Object[])
-        Map<String, Object[]> studentData
-                = new TreeMap<String, Object[]>();
-
-        studentData.put(
-                "1",
-                new Object[] { "Roll No", "NAME", "Year" });
-
-        studentData.put("2", new Object[] { "128", "Aditya",
-                "2nd year" });
-
-        studentData.put(
-                "3",
-                new Object[] { "129", "Narayana", "2nd year" });
-
-        studentData.put("4", new Object[] { "130", "Mohan",
-                "2nd year" });
-
-        studentData.put("5", new Object[] { "131", "Radha",
-                "2nd year" });
-
-        studentData.put("6", new Object[] { "132", "Gopal",
-                "2nd year" });
-
-        Set<String> keyid = studentData.keySet();
-
-        int rowid = 0;
-
-        // writing the data into the sheets...
-
-        for (String key : keyid) {
-
-            row = spreadsheet.createRow(rowid++);
-            Object[] objectArr = studentData.get(key);
-            int cellid = 0;
-
-            for (Object obj : objectArr) {
-                cell = row.createCell(cellid++);
-                cell.setCellValue((String)obj);
             }
-        }
+            return new ByteArrayInputStream(outputStream.toByteArray());
+        };
+        SimpleDateFormat df = new SimpleDateFormat("yyyyMMdd");
+        StreamResource resource = new StreamResource(source, "RendicionExport_"
+                        + cabecera.getCodComprobante() + "_"
+                        + df.format(new Date(System.currentTimeMillis()))
+                        + ".xlsx");
+        //resource.setMIMEType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8");
+        //resource.setMIMEType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+        resource.setMIMEType("application/xls");
 
-        File currDir = new File(".");
-        String path = currDir.getAbsolutePath();
-        String fileLocation = path.substring(0, path.length() - 1) + "temp.xlsx";
+        log.info("Resource: " + resource.getFilename() + " "
+                + resource.getMIMEType());
 
-        FileOutputStream outputStream = null;
-        try {
-            outputStream = new FileOutputStream(fileLocation);
-            workbook.write(outputStream);
-            workbook.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
 
+        FileDownloader fileDownloader = new FileDownloader(resource);
+        //UI.getCurrent().
+
+        //)fileDownloader.
+        Embedded emb = new Embedded();
+        emb.setSizeFull();
+        emb.setType(Embedded.TYPE_BROWSER);
+        emb.setSource(resource);
+
+        Window repWindow = new Window();
+        repWindow.setWindowMode(WindowMode.NORMAL);
+        repWindow.setWidth(700, Sizeable.Unit.PIXELS);
+        repWindow.setHeight(600, Sizeable.Unit.PIXELS);
+        repWindow.setPositionX(200);
+        repWindow.setPositionY(50);
+        repWindow.setModal(false);
+        repWindow.setContent(emb);
+        repWindow.setDraggable(true);
+        UI.getCurrent().addWindow(repWindow);
+        //UI.getCurrent().addWindow(fileDownloader);
+        //JavaScript.getCurrent().execute("window.onload = function() { window.print(); } ");
     }
 }
