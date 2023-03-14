68131a44d4e7647ff4c2bfc6a13a258b2e1602af
ab <a.borkowska@gmail.com>
2023-03-14 16:25:29 -0500

    Observaciones y detalles

--- sql/update_vsj_rendiciondetalle_set_cod_rendicioncabecera.sql
+++ sql/update_vsj_rendiciondetalle_set_cod_rendicioncabecera.sql
@@ -1,8 +1,8 @@
-UPDATE scp_rendiciondetalle
+UPDATE [dbo].[scp_rendiciondetalle]
 SET cod_rendicioncabecera=cab.cod_rendicioncabecera
 FROM
-    scp_rendicioncabecera cab,
-    scp_rendiciondetalle rend
+   [dbo].[scp_rendicioncabecera] cab,
+ [dbo].[scp_rendiciondetalle]rend
 WHERE rend.cod_mes=cab.cod_mes AND
       rend.cod_origen=cab.cod_origen AND
       rend.cod_comprobante=cab.cod_comprobante AND
