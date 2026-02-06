a6ec6a6988b7d983280514435d844f6e0729ad1e
Paweł Rubach <pawel.rubach@gmail.com>
2026-02-06 14:44:54 +0100

    Add list of required software

---
+++ winservice/install_prerequisites.bat
@@ -0,0 +1,7 @@
+choco install -y temurin8 notepadplusplus git maven wintail
+choco install intellijidea-community --version=2018.3.6 7zip
+choco install -y mysql mysql.workbench
+
+
+dism /online /Add-Capability /CapabilityName:WMIC
+
